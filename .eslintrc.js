987ef87310844ec55b0a4d9963d96c98b7ea12eb
gdedany <gdedanyaa@gmail.com>
2024-12-28 18:46:14 +0300

    adjusted eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
     browser: true,
     es6: true,
     "jest/globals": true,
+    node: true,
   },
   extends: ["eslint:recommended", "plugin:react/recommended"],
   parserOptions: {
@@ -17,8 +18,8 @@ module.exports = {
     indent: ["error", 2],
     "linebreak-style": ["error", "unix"],
     quotes: ["error", "double"],
-    semi: ["error", "never"],
-    eqeqeq: "error",
+    semi: ["off", "never"],
+    eqeqeq: "off",
     "no-trailing-spaces": "error",
     "object-curly-spacing": ["error", "always"],
     "arrow-spacing": ["error", { before: true, after: true }],
@@ -27,7 +28,7 @@ module.exports = {
   },
   settings: {
     react: {
-      version: "detect", 
+      version: "detect",
     },
   },
 };
